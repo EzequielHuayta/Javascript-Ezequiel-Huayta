cc0ee7c20bc2df71d46b6b1d29d4e43aabb78533
ezequiel <ezehuayta02@gmail.com>
2022-06-29 13:07:40 -0300

    comente lo demas para mostrar lo del array nada mas. Desafio Incorporar Arrays

--- index.js
+++ index.js
@@ -2,17 +2,48 @@
 //Login, lo hice pensando en que va a volver a tocar el boton y va a volver a llamar a la funcion con el nuevo usuario y password, por eso no vuelve a llamar a los prompt
 let usuario = window.prompt('Ingrese usuario (admin)');
 let password = window.prompt('Ingrese contraseña (admin)');
-login();
 
-function login(usuario,password){
+class product{
+    constructor(id, product, price, stock){
+        this.id = id;
+        this.name = product
+        this.price = price;
+        this.vendido = false;
+        this.stock = parseInt(stock)
+    }
+}
+const products = [];
+
+const login = (usuario,password) => {
     if(usuario !== 'admin' || password !== 'admin'){
         window.alert('Datos incorrectos, por favor volver a ingresar los datos')
+        return true // por algo siempre devuelve true para no generar confusiones
     }
     else{
         window.alert('Ingreso con exito!')
+        return true // por algo siempre devuelve true para no generar confusiones
+    }
+    return true; // por algo siempre devuelve true para no generar confusiones
+};
+
+createProduct(login);
+
+function createProduct(login){
+    if(login){
+        for ( let i = 1; i <= 2; i++){
+        let name = window.prompt(`Ingrese producto (2 en total) ) (Dar de alto el producto)`);
+        let price = parseInt(window.prompt(`Ingresar precio del producto`));
+        let stock = parseInt(window.prompt(`Ingresar stock del producto`));
+        products.push (new product(i, name, price, stock))
+        }
+    }
+    for (const pr of products){
+    console.log(pr.name)
+    window.alert(`${pr.name} ${pr.price} ${pr.stock}`)
     }
 }
 
+/*
 //Añadir al carrito (sumar productos)
 let total = 0
 let price = 0;
@@ -21,7 +52,7 @@ do{
     if(price === ''){
         window.alert(`Por favor no dejar vacio, ingrese algo.\nIngrese 0 para salir`);
     }else{
-        price = parseInt(window.prompt(`Ingresar precio del producto.\nIngrese 0 para salir`));
+        price = parseInt(window.prompt(`Añadir al carrito (precio del producto agregado).\nIngrese 0 para salir`));
         cart(price)
     }
 }while(price!== 0)
@@ -48,3 +79,4 @@ const cuotas = (cuotas) =>{
 }
 let cantCuotas = parseInt(window.prompt(`Ingresar cuantas cuotas quiere`));
 cuotas(cantCuotas);
+*/
